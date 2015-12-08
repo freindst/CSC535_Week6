eddfee8a0305ec7408d8cfd930ccc05ec53c109e
blade <yiming.liao@cuw.edu>
2015-12-07 23:57:45 -0600

    update

--- app/src/main/java/com/example/frein/json/JSON_core.java
+++ app/src/main/java/com/example/frein/json/JSON_core.java
@@ -6,9 +6,5 @@ import java.util.Stack;
  * Created by frein on 12/6/2015.
  */
 public class JSON_core {
-    static AddObjActivity currentObjActivity;
-    static String theJSON = "";
-    static String currentString = "";
     static Stack<String> stackJSON;
-
 }
